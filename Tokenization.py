360894a8cd335a562d219c8c0f49950365d24cbb
VINAYAK Naveen <naveen.vinayak-extern@rntbci.com>
2021-09-15 22:41:54 +0530

    sentence and word tokenization

---
+++ Tokenization.py
@@ -0,0 +1,80 @@
+# https://www.analyticsvidhya.com/blog/2020/05/what-is-tokenization-nlp/#:~:text=Tokens%20are%20the%20building%20blocks,n%2Dgram%20characters)%20tokenization.
+''' Highlights
+Tokenization is a key (and mandatory) aspect of working with text data
+We’ll discuss the various nuances of tokenization, including how to handle Out-of-Vocabulary words (OOV)
+
+ A Quick Rundown of Tokenization
+Tokenization is a common task in Natural Language Processing (NLP). It’s a fundamental step in both traditional NLP methods like Count Vectorizer and Advanced Deep Learning-based architectures like Transformers.
+
+Tokens are the building blocks of Natural Language.
+
+Tokenization is a way of separating a piece of text into smaller units called tokens. Here, tokens can be either words, characters, or subwords. Hence, tokenization can be broadly classified into 3 types – word, character, and subword (n-gram characters) tokenization.
+
+For example, consider the sentence: “Never give up”.
+
+The most common way of forming tokens is based on space. Assuming space as a delimiter, the tokenization of the sentence results in 3 tokens – Never-give-up. As each token is a word, it becomes an example of Word tokenization.
+
+Similarly, tokens can be either characters or subwords. For example, let us consider “smarter”:
+
+Character tokens: s-m-a-r-t-e-r
+Subword tokens: smart-er
+
+'''
+import nltk
+#nltk.download()
+
+# called as paragraph or corps
+
+paragraph = """I have three visions for India. In 3000 years of our history, people from all over 
+               the world have come and invaded us, captured our lands, conquered our minds. 
+               From Alexander onwards, the Greeks, the Turks, the Moguls, the Portuguese, the British,
+               the French, the Dutch, all of them came and looted us, took over what was ours. 
+               Yet we have not done this to any other nation. We have not conquered anyone. 
+               We have not grabbed their land, their culture, 
+               their history and tried to enforce our way of life on them. 
+               Why? Because we respect the freedom of others.That is why my 
+               first vision is that of freedom. I believe that India got its first vision of 
+               this in 1857, when we started the War of Independence. It is this freedom that
+               we must protect and nurture and build on. If we are not free, no one will respect us.
+               My second vision for India’s development. For fifty years we have been a developing nation.
+               It is time we see ourselves as a developed nation. We are among the top 5 nations of the world
+               in terms of GDP. We have a 10 percent growth rate in most areas. Our poverty levels are falling.
+               Our achievements are being globally recognised today. Yet we lack the self-confidence to
+               see ourselves as a developed nation, self-reliant and self-assured. Isn’t this incorrect?
+               I have a third vision. India must stand up to the world. Because I believe that unless India 
+               stands up to the world, no one will respect us. Only strength respects strength. We must be 
+               strong not only as a military power but also as an economic power. Both must go hand-in-hand. 
+               My good fortune was to have worked with three great minds. Dr. Vikram Sarabhai of the Dept. of 
+               space, Professor Satish Dhawan, who succeeded him and Dr. Brahm Prakash, father of nuclear material.
+               I was lucky to have worked with all three of them closely and consider this the great opportunity of my life. 
+               I see four milestones in my career"""
+
+# Tokenizing sentences
+
+# converts paragraph into sentences.
+sentences = nltk.sent_tokenize(paragraph)
+print(sentences)
+
+# Tokenizing words
+
+# can convert paragraph into words
+word = nltk.word_tokenize(paragraph)
+print(word)
+
+print("\n")
+print("*"*50)
+print("\n")
+
+# can convert sentences into words
+sentence_word=[]
+for sent in sentences:
+    sent_word = nltk.word_tokenize(sent)
+    print(sent_word)
+    for i in sent_word:
+        sentence_word.append(i)
+
+print("\n")
+print("*"*50)
+print("\n")
+
+print("output :-",sentence_word)
